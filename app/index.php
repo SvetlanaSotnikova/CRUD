a49cfa1c679fc351af6e728924b6efe773aef968
Svetlana Sotnikova <sotnikovas337@gmail.com>
2024-07-18 20:37:49 +0300

    upd

--- app/index.php
+++ app/index.php
@@ -28,91 +28,91 @@
                     </thead>
 
                     <tbody>
-                        <?php foreach ($result as $value) { ?>
-                        <tr>
-                            <td><?php echo $value -> id;  ?></td>
-                            <td><?php echo $value -> name; ?></td>
-                            <td><?php echo $value -> email;?></td>
-                            <td><a href="?id = <?php echo $value -> id;?>" class="btn btn-success"
-                                    data-bs-toggle="modal" data-bs-target="#edit<?php echo $value -> id;?>"><i
-                                        class="bi bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                            height="16" fill="currentColor" class="bi bi-pencil-square"
-                                            viewBox="0 0 16 16">
-                                            <path
-                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                            <path fill-rule="evenodd"
-                                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                                        </svg></i></a>
-                                <a href="?id = <?php echo $value -> id;?>" class="btn btn-danger" data-bs-toggle="modal"
-                                    data-bs-target="#delete<?php echo $value -> id;?>"><i class="bi bi-trash"><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                            <path
-                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                                            <path
-                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                                        </svg></i></a>
-                            </td>
-                        </tr>
-                        <!-- modal edit-->
-                        <div class="modal fade" id="edit<?php echo $value -> id;?>" tabindex="-1"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Change a note</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <form action="?id=<?php echo $value->id; ?>" method="post">
-                                            <div class="form-group">
-                                                <small>Name</small>
-                                                <input type="text" class="form-control" name="name"
-                                                    value="<?php echo $value -> name;?>">
-                                            </div>
-                                            <div class="form-group">
-                                                <small>Email</small>
-                                                <input type="text" class="form-control" name="email"
-                                                    value="<?php echo $value -> email;?>">
-                                            </div>
+                        <?php foreach ($result as $value) : ?>
+                            <tr>
+                                <td><?= $value -> id;  ?></td>
+                                <td><?= $value -> name; ?></td>
+                                <td><?= $value -> email;?></td>
+                                <td><a href="?id = <?= $value -> id;?>" class="btn btn-success"
+                                        data-bs-toggle="modal" data-bs-target="#edit<?= $value -> id;?>"><i
+                                            class="bi bi-pencil-square"><svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                height="16" fill="currentColor" class="bi bi-pencil-square"
+                                                viewBox="0 0 16 16">
+                                                <path
+                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                <path fill-rule="evenodd"
+                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                            </svg></i></a>
+                                    <a href="?id = <?= $value -> id;?>" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#delete<?= $value -> id;?>"><i class="bi bi-trash"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                                <path
+                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                            </svg></i></a>
+                                </td>
+                            </tr>
+                            <!-- modal edit-->
+                            <div class="modal fade" id="edit<?= $value -> id;?>" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Change a note</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form action="?id=<?= $value->id; ?>" method="post">
+                                                <div class="form-group">
+                                                    <small>Name</small>
+                                                    <input type="text" class="form-control" name="name"
+                                                        value="<?= $value -> name;?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <small>Email</small>
+                                                    <input type="text" class="form-control" name="email"
+                                                        value="<?= $value -> email;?>">
+                                                </div>
 
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary"
-                                            data-bs-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-primary" name="edit">Save</button>
-                                        </form>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                        <!-- modal edit-->
-                        <!-- modal delete-->
-                        <div class="modal fade" id="delete<?php echo $value->id; ?>" tabindex="-1"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Delete a note No.
-                                            <?php echo $value->id; ?>?</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <form action="?id=<?php echo $value->id; ?>" method="post">
+                                        </div>
+                                        <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary"
                                                 data-bs-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-danger" name="delete">Delete</button>
-                                        </form>
+                                            <button type="submit" class="btn btn-primary" name="edit">Save</button>
+                                            </form>
+                                        </div>
+
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- modal delete-->
+                            <!-- modal edit-->
+                            <!-- modal delete-->
+                            <div class="modal fade" id="delete<?= $value->id; ?>" tabindex="-1"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Delete a note No.
+                                                <?= $value->id; ?>?</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <form action="?id=<?= $value->id; ?>" method="post">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-danger" name="delete">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- modal delete-->
 
-                        <?php } ?>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
