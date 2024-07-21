57fbb809a922ee47ce402e3282c22434170594bd
Svetlana Sotnikova <sotnikovas337@gmail.com>
2024-07-22 01:35:17 +0300

    upd

--- app/controller/controller.php
+++ app/controller/controller.php
@@ -42,16 +42,99 @@ if (isset($_POST['edit'])) {
 
 // delete
 if (isset($_POST['delete'])) {
+    
     $result = deleteUser($get_id);
     if($result) {
         header("Location: " . $_SERVER['HTTP_REFERER']);
     } else {
         $error = "Failed to delete user :(";
     }
-    // $sql = ("UPDATE user SET flag = 1 WHERE id = ?");
-    // $query = $pdo->prepare($sql);
-    // $query -> execute([$get_id]);
-    // if ($query) {
-    //     header("Location: " . $_SERVER['HTTP_REFERER']);
-    // }
+
 }
+
+function handleRequest() {
+    $route = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $content = null;
+    $fullPage = null;
+    $title = null;
+
+    switch ($route) {
+        case '/':
+            $content = renderTemplate('./views/list.php', [
+                'title' => 'Главная',
+                'description' => 'CRUD Приложение',
+                'users' => getUsers(),
+            ]);
+            break;
+            
+        case '/update':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'], $_POST['name'], $_POST['email'])) {
+                updateUser($_POST['id'], $_POST['email'], $_POST['name']);
+                
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }
+            break;
+
+        case '/create':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name'], $_POST['email'])) {
+                createUser($_POST['email'], $_POST['name']);
+                
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }
+            break;
+
+        case '/delete':
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+                deleteUser($_POST['id']);
+                
+                header("Location: " . $_SERVER['HTTP_REFERER']);
+                exit();
+            }
+            break;
+
+        default:
+            $content = renderTemplate('./views/404.php', [
+                'title' => '404',
+                'description' => 'Страницы не существует',
+            ]);
+            break;
+    }
+
+    if (!is_null($content)) {
+        $fullPage = renderTemplate('./views/mainLayout.php', [
+            'title' => $title,
+            'content' => $content
+            // 'users' => getUsers(),
+        ]);
+
+        return $fullPage;
+    }
+}
+
+
+function init() {
+    echo handleRequest();
+}
+
+function renderTemplate($templatePath, $variables = []) {
+
+    // Извлекаем переменные из массива
+    extract($variables);
+    
+    // Включаем буферизацию вывода
+    ob_start();
+    
+    // Подключаем файл шаблона
+    if (file_exists($templatePath)) {
+        require $templatePath;
+    } else {
+        echo "Файл шаблона не найден: $templatePath";
+    }
+    
+    // Получаем содержимое буфера
+    $content = ob_get_clean();
+    
+    return $content;
+  }
--- app/index.php
+++ app/index.php
@@ -2,4 +2,5 @@
     include "config/config.php";
     include "functions/functions.php";
     include "controller/controller.php";
-    include "views/list.php";
+    // include "views/list.php";
+    init();
